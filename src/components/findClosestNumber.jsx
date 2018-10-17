fb03f2abe6aae5e2dc0413b23fa3a7592a8451cc
Asko Kriiska <asko.kriiska@gmail.com>
2018-10-17 16:20:13 +0300

    catch empty string

--- src/components/findClosestNumber.jsx
+++
@@ -1,5 +0,0 @@
-function FindClosestNumber(props) {
-  return indexClosest;
-}
-
-export default FindClosestNumber;
--- src/components/windStringComposer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FindClosestNumber from "./findClosestNumber";
-
-const WindString = ({ direction, windSpeed }) => {
-  return windString;
-};
-
-export default WindString;
