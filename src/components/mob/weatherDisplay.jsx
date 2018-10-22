b3bef57733315e17b915a2526d959ace7cea811d
Asko Kriiska <asko.kriiska@gmail.com>
2018-10-22 16:36:02 +0300

    landscape cellphone experiments

--- src/components/mob/weatherDisplay.jsx
+++ src/components/mob/weatherDisplay.jsx
@@ -29,20 +29,19 @@ const WeatherDisplayMob = ({
         <YahooLink link={weatherLink} />
       </MediaQuery>
       <MediaQuery query="(orientation: landscape)">
-        <span className="results results-city">{city}:</span>
         <div className="d-flex flex-row justify-content-center">
-          <div className="flex-column d-flex justify-content-center">
-            <div>
-              <span className="results results-temperature">{tempString}</span>
-            </div>
-            <div>
-              <span className="results results-wind">{windString}</span>
-            </div>
+          <div>
+            <span className="results results-city">{city}:</span>
+          </div>
+          <div>
+            <span className="results results-temperature">{tempString}</span>
+          </div>
+          <div>
+            <span className="results results-wind">{windString}</span>
           </div>
           <div>
             <img src={imagelink} alt={text} />
           </div>
-          <div />
         </div>
         <YahooLink link={weatherLink} />
       </MediaQuery>
