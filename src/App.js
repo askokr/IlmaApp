fc7f8c6b236846f8672a53c8af43802f1a721aeb
Asko Kriiska <asko.kriiska@gmail.com>
2018-10-22 15:58:11 +0300

    input higher on cellphone

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{ overflow: "hidden", width: "100%" }}>
+      <div style={{ width: "100%" }}>
         <div>
           <header className="App-header">
             <h1>IlmaÄpp</h1>
